7d36b95857277c369e5a07893964e0f2af4de869
kyeongho <dagh1218@naver.com>
2022-10-27 00:56:26 +0900

    simple-board-project #12 Article entity 구현
    - 기생성된 Article 도메인 객체를 entity화
    - index 설정
    -- 원활한 index 설정을 위해 content size 변경
    - 각 column 설정 반영
    - Jpa 설정 관련 클래스 생성
    -- JpaAuditing 설정 추가

--- simple-board/src/main/java/com/guardjo/simpleboard/domain/Article.java
+++ simple-board/src/main/java/com/guardjo/simpleboard/domain/Article.java
@@ -1,14 +1,78 @@
 package com.guardjo.simpleboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(name = "title", columnList = "title"),
+        @Index(name = "hashtag", columnList = "hashtag"),
+        @Index(name = "creator", columnList = "creator"),
+        @Index(name = "createTime", columnList = "createTime")
+})
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Setter
+    @Column(nullable = false)
     private String title;
+
+    @Setter
+    @Column(nullable = false, length = 5000)
     private String content;
+
+    @Setter
     private String hashtag;
+
+    @CreatedBy
     private String creator;
+
+    @CreatedDate
     private LocalDateTime createTime;
+
+    @LastModifiedBy
     private String modifier;
+
+    @LastModifiedDate
     private LocalDateTime modifiedTime;
+
+    protected Article() {
+
+    }
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return id == article.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
