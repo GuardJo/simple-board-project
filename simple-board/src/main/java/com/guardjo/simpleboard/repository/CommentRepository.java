ff95b759a55b776ceea0e68720eb1ba19ed478bb
kyeongho <dagh1218@naver.com>
2022-10-31 01:05:46 +0900

    simple-board-project #1 article, comment 관련 검색 api 구현
    - QuerydslPrecatedExecutor를 활용하여 기본 검색 API 추가
    - QuerydslBinderCustomizer를 활용하여 검색 필터링 커스터마이징

--- simple-board/src/main/java/com/guardjo/simpleboard/repository/CommentRepository.java
+++ simple-board/src/main/java/com/guardjo/simpleboard/repository/CommentRepository.java
@@ -1,9 +1,26 @@
 package com.guardjo.simpleboard.repository;
 
 import com.guardjo.simpleboard.domain.Comment;
+import com.guardjo.simpleboard.domain.QComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>,
+        QuerydslPredicateExecutor<Comment>, QuerydslBinderCustomizer<QComment> {
+    @Override
+    default void customize(QuerydslBindings bindings, QComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.hashtag, root.creator, root.createTime);
+
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.creator).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createTime).first(DateTimeExpression::eq);
+    }
 }
