02ece1aa18c975a0622f43ac245336a2d3e6e3a2
kyeongho <dagh1218@naver.com>
2022-11-01 02:19:52 +0900

    simple-board-project #15 Article 목록 반환 view 뼈대 구현
    - Article 목록 view 반환을 위한 controller mapping 구현
    - Article 목록 view 를 위한 html 작성
    - thymleaf 템플릿 엔진을 적용하여 일부 요소 별도 분리

--- simple-board/src/main/java/com/guardjo/simpleboard/controller/ArticleController.java
+++ simple-board/src/main/java/com/guardjo/simpleboard/controller/ArticleController.java
@@ -1,9 +1,18 @@
 package com.guardjo.simpleboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
+    @GetMapping
+    public String findAllArticles(ModelMap modelMap) {
+        modelMap.addAttribute("articles", List.of());
+        return "article/index";
+    }
 }
