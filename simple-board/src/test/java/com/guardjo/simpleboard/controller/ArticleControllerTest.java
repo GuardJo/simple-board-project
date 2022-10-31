c9ba93cf0f853a5b4adc7276ecde45ddbbd2707d
kyeongho <dagh1218@naver.com>
2022-10-31 23:28:03 +0900

    simple-board-project #15 api 설계에 따른 view 반환 테스트 코드 작성
    - api 설계에 따른 view 반환 테스트 코드 작성
    - api 설계에 따른 view controller 생성 (ArticleController)

---
+++ simple-board/src/test/java/com/guardjo/simpleboard/controller/ArticleControllerTest.java
@@ -0,0 +1,56 @@
+package com.guardjo.simpleboard.controller;
+
+import com.guardjo.simpleboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+    private final MockMvc mockMvc;
+
+    ArticleControllerTest(@Autowired  MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("Article 목록 페이지 반환 테스트")
+    @Test
+    void testGetArticleView() throws Exception {
+        mockMvc.perform(get("/article"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("특정 Artile 페이지 반환 테스트")
+    @Test
+    void testGetArticleDetailView() throws Exception {
+        mockMvc.perform(get("/article/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("comments"));
+    }
+
+    @DisplayName("검색 페이지(임시) 반환 테스트")
+    @Test
+    void testGetArticleSearchView() throws Exception {
+        mockMvc.perform(get("/article/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("Hashtag 검색 페이지(임시) 반환 테스트")
+    @Test
+    void testGetArticleHashtagSearchView() throws Exception {
+        mockMvc.perform(get("/article/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+}
