e3f49f9211b7ed08b50d24582d8cd73c0dac8609
kyeongho <dagh1218@naver.com>
2022-11-02 02:31:14 +0900

    simple-board-project #15 articleController 내 단일 article 반환 추가
    - articleController 내 단일 article 반환 api 추가
    - 단일 게시글 페이지 작성 및 테스트
    -- thymeleaf 설정 분리

--- simple-board/src/main/java/com/guardjo/simpleboard/controller/ArticleController.java
+++ simple-board/src/main/java/com/guardjo/simpleboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.guardjo.simpleboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,5 +15,13 @@ public class ArticleController {
     public String findAllArticles(ModelMap modelMap) {
         modelMap.addAttribute("articles", List.of());
         return "article/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String findArticle(@PathVariable Long articleId, ModelMap modelMap) {
+        modelMap.addAttribute("article", "test");
+        modelMap.addAttribute("comments", List.of());
+
+        return "article/detail";
     }
 }
