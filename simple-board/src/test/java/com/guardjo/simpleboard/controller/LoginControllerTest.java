5638eb9f777a3e98a2af27b5794781c36fb0beea
kyeongho <dagh1218@naver.com>
2022-11-03 01:24:46 +0900

    simple-board-project #15 spring security 를 활용하여 로그인 view 구현
    - spring security 에 기본적으로 탑재된 로그인 view 사용
    - 현재 인증/인가 관련 로직이 구현되지 않아 우선은 모든 요청에 대해 인증/인가 처리되도록 설정
    - login view 관련하여 테스트 코드 작성 및 테스트 진행

---
+++ simple-board/src/test/java/com/guardjo/simpleboard/controller/LoginControllerTest.java
@@ -0,0 +1,33 @@
+package com.guardjo.simpleboard.controller;
+
+import com.guardjo.simpleboard.config.SecurityConfig;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(SecurityConfig.class)
+@WebMvcTest
+public class LoginControllerTest {
+    private final MockMvc mockMvc;
+
+    public LoginControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("spring security 라이브러리로 추가된 login 페이지 출력 테스트")
+    @Test
+    void testLogin() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
+    }
+}
