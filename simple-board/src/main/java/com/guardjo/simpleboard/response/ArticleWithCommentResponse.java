d80d273dc1e643ea27ecf363b0fd26034efb0e78
kyeongho <dagh1218@naver.com>
2022-12-09 23:53:43 +0900

    #17 게시글 상세 페이지 뷰 연동
    - 게시글 상세 페이지 데이터 및 뷰 연동 완료
    - ArticleWithCommentResponse에 content 데이터 추가

--- simple-board/src/main/java/com/guardjo/simpleboard/response/ArticleWithCommentResponse.java
+++ simple-board/src/main/java/com/guardjo/simpleboard/response/ArticleWithCommentResponse.java
@@ -5,9 +5,9 @@ import com.guardjo.simpleboard.dto.ArticleWithCommentDto;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public record ArticleWithCommentResponse(Long id, ArticleResponse articleResponse, Set<CommentResponse> commentResponses) {
-    public static ArticleWithCommentResponse of(Long id, ArticleResponse articleResponse, Set<CommentResponse> commentResponses) {
-        return new ArticleWithCommentResponse(id, articleResponse, commentResponses);
+public record ArticleWithCommentResponse(Long id, ArticleResponse articleResponse, String content, Set<CommentResponse> commentResponses) {
+    public static ArticleWithCommentResponse of(Long id, ArticleResponse articleResponse, String content, Set<CommentResponse> commentResponses) {
+        return new ArticleWithCommentResponse(id, articleResponse, content, commentResponses);
     }
 
     public static ArticleWithCommentResponse from(ArticleWithCommentDto articleWithCommentDto) {
@@ -20,6 +20,7 @@ public record ArticleWithCommentResponse(Long id, ArticleResponse articleRespons
                         articleWithCommentDto.creator(),
                         articleWithCommentDto.createTime()
                 ),
+                articleWithCommentDto.content(),
                 articleWithCommentDto.commentDtos().stream().map(CommentResponse::from).collect(Collectors.toSet())
         );
     }
