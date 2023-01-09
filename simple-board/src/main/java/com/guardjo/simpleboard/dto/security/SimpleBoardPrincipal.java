e9dbe5260273d81c99c8219398ab9731924d7ec8
kyeongho <dagh1218@naver.com>
2023-01-10 00:06:23 +0900

    #34 spring security 계정 설정을 위한 객체 추가
    - UserDetails 객체 추가
    - 권한 설정을 위한 RoleType enum 추가

---
+++ simple-board/src/main/java/com/guardjo/simpleboard/dto/security/SimpleBoardPrincipal.java
@@ -0,0 +1,69 @@
+package com.guardjo.simpleboard.dto.security;
+
+import com.guardjo.simpleboard.dto.MemberDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record SimpleBoardPrincipal(String email, String name, String password,
+                                   Collection<? extends GrantedAuthority> authorities) implements UserDetails {
+    public static SimpleBoardPrincipal of(String email, String name, String password) {
+        Set<RoleType> roleTypes = Set.of(RoleType.USER);
+        return new SimpleBoardPrincipal(
+                email,
+                name,
+                password,
+                roleTypes.stream()
+                        .map(RoleType::getName)
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toUnmodifiableList()));
+    }
+
+    public static SimpleBoardPrincipal from(MemberDto memberDto) {
+        return SimpleBoardPrincipal.of(
+                memberDto.email(),
+                memberDto.name(),
+                memberDto.password()
+        );
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
