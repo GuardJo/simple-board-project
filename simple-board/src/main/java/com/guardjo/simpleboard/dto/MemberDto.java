d13f30926f76f02307856ad81e42066d335e0002
kyeongho <dagh1218@naver.com>
2023-01-10 00:05:41 +0900

    #34 Member Entity -> MemberDto 변환 메소드 추가
    - 계정 정보 관련 객체 DTO 변환 메소드 추가

--- simple-board/src/main/java/com/guardjo/simpleboard/dto/MemberDto.java
+++ simple-board/src/main/java/com/guardjo/simpleboard/dto/MemberDto.java
@@ -1,5 +1,7 @@
 package com.guardjo.simpleboard.dto;
 
+import com.guardjo.simpleboard.domain.Member;
+
 import java.time.LocalDateTime;
 
 /**
@@ -9,5 +11,16 @@ public record MemberDto(Long id, String creator, LocalDateTime createTime, Strin
                         String password) {
     public static MemberDto of(Long id, String creator, LocalDateTime createTime, String email, String name, String password) {
         return new MemberDto(id, creator, createTime, email, name, password);
+    }
+
+    public static MemberDto from(Member member) {
+        return MemberDto.of(
+                member.getMemberId(),
+                member.getCreator(),
+                member.getCreateTime(),
+                member.getEmail(),
+                member.getName(),
+                member.getPassword()
+        );
     }
 }
