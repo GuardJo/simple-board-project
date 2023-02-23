7be9d1643ee765a35211a7ac4509b574a7a62914
kyeongho <dagh1218@naver.com>
2023-02-24 00:30:54 +0900

    #57 javadoc 문서 주석을 swagger Operation 변경되도록 구성
    - javadoc 문서 -> swagger 문서 변환을 위한 openapi-javadoc 의존 추가
    - 관련 테스트용 rest api 추가 및 javadoc 형식의 주석으로 테스트

--- simple-board/src/main/java/com/guardjo/simpleboard/controller/ExternalController.java
+++ simple-board/src/main/java/com/guardjo/simpleboard/controller/ExternalController.java
@@ -2,6 +2,7 @@ package com.guardjo.simpleboard.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,15 @@ public class ExternalController {
     @Operation(summary = "test api")
     public String test() {
         return "Test Successes";
+    }
+
+    /**
+     * openapi-javadoc 라이브러리를 통해 javadoc 주석을 swagger 문서화
+     * @param 테스트용 parameter (nullable)
+     * @return 테스트용
+     */
+    @GetMapping("/javadoc-test")
+    public String test2(@Nullable String param) {
+        return "Openapi javadoc test Successes " + param ;
     }
 }
