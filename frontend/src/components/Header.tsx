05684d6516f863508f3642222da6420886d3a031
Kyeongho <kyeongho_jo@lotte.net>
2024-05-09 16:06:45 +0900

    #69 Header 컴포넌트 모바일 환경 대응 개선
    - 모바일 화면에서 Header 컴포넌트에 대한 반응형 Dialog 구성

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Bars3Icon } from "@heroicons/react/24/outline";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
@@ -14,8 +15,18 @@ export default function Header() {
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <Link href='/' className="-m-1.5 p-1.5">
-                        <span className="h-9 w-auto">Simple Board</span>
+                        <span className="h-9 w-auto font-bold sm:text-xl">Simple Board</span>
                     </Link>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        onClick={() => setMobileMenuOpen(true)}
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    </button>
                 </div>
                 <div className="hidden lg:flex lg:gap-x-12">
                     <Link href='/' className="text-sm font-semibold leading-6 text-gray-900">Home</Link>
@@ -30,7 +41,39 @@ export default function Header() {
                         <Link href='/logout' onAbort={() => setIsLogin(false)} className="text-sm font-semibold leading-6 text-gray-900">Logout</Link>
                     }
                 </div>
+                <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                    <div className="flex items-center justify-between">
+                        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                            <div className="flex items-center justify-between">
+                                <Link href="/" className="-m-1.5 p-1.5">
+                                    <span className="sr-only">Simple Board</span>
+                                </Link>
+                                <button className="rounded-md text-gray-700"
+                                    onClick={() => setMobileMenuOpen(false)}
+                                >
+                                    <span className="sr-only">Close</span>
+                                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                                </button>
+                            </div>
+                            <div className="mt-6 flow-root">
+                                <div className="-my-6 divide-y divide-gray-500/10">
+                                    <Link href="/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</Link>
+                                    <Link href="/searcg-hashtag" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">hashtag</Link>
+                                </div>
+                            </div>
+                            <div className="py-6 mt-10">
+                                {(!isLogin) ?
+                                    <div className="flex gap-2">
+                                        <Link href='/login' onClick={() => setIsLogin(true)} className="text-sm font-semibold leading-6 text-gray-900">Login</Link>
+                                        <Image alt='kakao login' src='/images/kakao_login_small.png' width={50} height={20} />
+                                    </div> :
+                                    <Link href='/logout' onAbort={() => setIsLogin(false)} className="text-sm font-semibold leading-6 text-gray-900">Logout</Link>
+                                }
+                            </div>
+                        </DialogPanel>
+                    </div>
+                </Dialog>
             </nav>
         </header>
-    )
+    );
 }
