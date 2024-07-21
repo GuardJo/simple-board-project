5d29bd2660e6aced1957b6253c4b2d778be77dc9
kyeongho <dagh1218@naver.com>
2024-07-21 17:02:44 +0900

    #70 해시태그 검색 페이지 레이아웃 구성
    - 해시태그 검색 페이지 기본 레이아웃 구성
    - Header 컴포넌트 내 해시태그 검색 url 변경
    - 개발 환경 편의성을 위한 db 컨테이너 실행용 Makefile 구성

---
+++ frontend/src/app/articles/search-hashtags/page.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react";
+import Loading from './../../loading';
+import ArticleTable from "@/components/ArticleTable";
+
+export default function getSearchHashtagPage({ searchParams }: SearchHashtagParams) {
+    return (
+        <div className="flex flex-auto flex-col justify-center items-center gap-5">
+            <p>해시태그 목록 보여줄것</p>
+            <Suspense fallback={Loading()}>
+                <ArticleTable></ArticleTable>
+            </Suspense>
+        </div>
+    )
+}
+
+interface SearchHashtagParams {
+    searchParams: {
+        page?: number,
+        searchType?: string,
+        searchValue?: string,
+    },
+}
