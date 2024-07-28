061656afce79a90cc62ee08acb61b5a0e8c81c16
kyeongho <dagh1218@naver.com>
2024-07-28 22:41:50 +0900

    #71 게시글 본문 출력 컴포넌트 구현
    - 게시글 본문 내용 출력을 위한 ArticleContentForm 컴포넌트 구현
    - 관련 스토리북 구성

---
+++ frontend/src/stories/ArticleContentForm.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ArticleContentForm from '../components/ArticleContentForm';
+
+const meta = {
+    title: "components/ArticleContentForm",
+    component: ArticleContentForm,
+} satisfies Meta<typeof ArticleContentForm>
+
+export default meta;
+
+type Story = StoryObj<typeof ArticleContentForm>
+
+export const NoContents: Story = {
+    args: {}
+};
+
+export const HasContents:Story = {
+    args: {
+        content: "테스트 게시글",
+    },
+};
