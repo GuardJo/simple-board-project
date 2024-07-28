09ca9d4de4de608e34d9f00e4f847e2324240d4a
kyeongho <dagh1218@naver.com>
2024-07-28 23:56:55 +0900

    #71 댓글 출력 컴포넌트 구현
    - 게시글 별 댓글 내용 출력을 위한 ArticleComment 컴포넌트 구현
    - 관련 스토리북 구성

---
+++ frontend/src/components/ArticleComment.tsx
@@ -0,0 +1,17 @@
+export default function ArticleComment({ author, updatedAt, content }: ArticleCommentParam) {
+    return (
+        <div className="flex flex-col py-3">
+            <dt className="flex gap-1 justify-between">
+                <p className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{author}</p>
+                <p className="mb-1 text-gray-400 md:text-xs dark:text-gray-300">{updatedAt}</p>
+            </dt>
+            <dd className="text-sm font-semibold">{content}</dd>
+        </div>
+    )
+}
+
+interface ArticleCommentParam {
+    author: String,
+    updatedAt: String,
+    content: String,
+};
