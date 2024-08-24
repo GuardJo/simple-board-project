2acada7e7c49fd5092603e6513e1d073a252962c
kyeongho <dagh1218@naver.com>
2024-08-24 15:15:21 +0900

    #77 비밀번호 입력을 위한 TextInput 컴포넌트 개선
    - 신규 속성을 추가하여 해당 속성에 따라 비밀번호 type으로 렌더링되도록 개선
    - 관련 스토리북 추가

--- frontend/src/components/TextInput.tsx
+++ frontend/src/components/TextInput.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-export default function TextInput({ labelName, placeholder, onChange = () => { } }: TextInputParams) {
+export default function TextInput({ labelName, placeholder, onChange = () => { }, isSecret = false }: TextInputParams) {
     return (
         <div className="mb-6">
             <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{labelName}</label>
-            <input type="text" id="default-input" onChange={(e) => onChange(e.target.value)} placeholder={placeholder} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <input type={isSecret ? "password" : "text"} id="default-input" onChange={(e) => onChange(e.target.value)} placeholder={placeholder} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
     )
 }
@@ -14,4 +14,5 @@ interface TextInputParams {
     placeholder: string,
     content?: string,
     onChange?: Function,
+    isSecret?: Boolean,
 };
--- frontend/src/stories/TextInput.stories.ts
+++ frontend/src/stories/TextInput.stories.ts
@@ -15,8 +15,19 @@ export const Default: Story = {
     args: {
         labelName: "제목",
         placeholder: "제목을 입력해 주세요.",
-        onChange: (e: ChangeEvent<HTMLInputElement>) => {
-            action("handleChange")(`Changed Value : ${e.target.value}`);
-        }
+        onChange: (e: ChangeEvent<HTMLInputElement>) => (
+            action("handleChange")(`Changed Value : ${e.target.value}`)
+        ),
+    },
+};
+
+export const Password: Story = {
+    args: {
+        labelName: "비밀번호",
+        placeholder: "비밀번호를 입력해 주세요.",
+        onChange: (e: ChangeEvent<HTMLInputElement>) => (
+            action("handleChange")(`Changed Value : ${e.target.value}`)
+        ),
+        isSecret: true,
     },
 };
