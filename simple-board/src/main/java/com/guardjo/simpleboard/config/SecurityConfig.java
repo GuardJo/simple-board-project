4b9a4b116e5e6fcfdec63d78a0ed49bfd39a5aef
Kyeongho <dagh1218@naver.com>
2024-09-16 18:37:40 +0900

    #77 로그인 후 접근 가능 페이지 별도 체크 기능 구성
    - 게시글 생성 API의 경우 인증 작업 이후 요청할 수 있도록 수정
    - 게시글 작성 페이지 접근 시 로그인하지 않았을 경우 로그인 페이지로 이동하도록 개선
    - 게시글 작성 후 신규 게시글까지 refresh 되도록 루트 경로로 이동하도록 개선

--- simple-board/src/main/java/com/guardjo/simpleboard/config/SecurityConfig.java
+++ simple-board/src/main/java/com/guardjo/simpleboard/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.guardjo.simpleboard.config;
 
-import java.util.List;
-
+import com.guardjo.simpleboard.api.UrlContext;
+import com.guardjo.simpleboard.dto.MemberDto;
+import com.guardjo.simpleboard.dto.security.SimpleBoardPrincipal;
+import com.guardjo.simpleboard.response.KakaoOAuth2UserResponse;
+import com.guardjo.simpleboard.service.MemberService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -21,100 +24,95 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
-import com.guardjo.simpleboard.api.UrlContext;
-import com.guardjo.simpleboard.dto.MemberDto;
-import com.guardjo.simpleboard.dto.security.SimpleBoardPrincipal;
-import com.guardjo.simpleboard.response.KakaoOAuth2UserResponse;
-import com.guardjo.simpleboard.service.MemberService;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
-	private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
-		OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService) throws Exception {
-		httpSecurity.authorizeRequests((auth) ->
-				auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-					.permitAll()
-					.mvcMatchers(
-						HttpMethod.GET,
-						"/",
-						"/article",
-						"/article/search-hashtag"
-					).permitAll()
-					.mvcMatchers("/api/**")
-					.permitAll()
-					.anyRequest().authenticated()
-			).formLogin(configure -> {
-				configure.loginPage("/login")
-					.permitAll()
-					.loginProcessingUrl(UrlContext.LOGIN_URL)
-					.failureHandler((request, response, exception) -> response.setStatus(HttpStatus.UNAUTHORIZED.value()))
-					.permitAll();
-			})
-			.logout(logOutConfig -> {
-				logOutConfig.logoutSuccessUrl("/")
-					.permitAll()
-					.logoutUrl(UrlContext.LOGOUT_URL)
-					.permitAll();
-			})
-			.oauth2Login(config -> config.userInfoEndpoint(
-				oAuth2 -> oAuth2.userService(oAuth2UserService)
-			))
-			.csrf(configure -> configure.ignoringAntMatchers("/api/**"))
-			.cors(httpSecurityCorsConfigurer -> {
-				httpSecurityCorsConfigurer.configurationSource(request -> {
-					CorsConfiguration configuration = new CorsConfiguration();
-					// TODO cors 허용 url 별도 환경변수화 시키기
-					configuration.setAllowedOrigins(List.of(
-						"http://localhost:8080",
-						"http://localhost:3000"
-					));
-					configuration.setAllowedMethods(List.of(CorsConfiguration.ALL));
-					configuration.setAllowCredentials(true);
-					configuration.setAllowedHeaders(List.of(CorsConfiguration.ALL));
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
+                                                   OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService) throws Exception {
+        httpSecurity.authorizeRequests((auth) ->
+                        auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                                .permitAll()
+                                .mvcMatchers(
+                                        HttpMethod.GET,
+                                        "/",
+                                        "/article",
+                                        "/article/search-hashtag",
+                                        "/api/v2/**"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                ).formLogin(configure -> {
+                    configure.loginPage("/login")
+                            .permitAll()
+                            .loginProcessingUrl(UrlContext.LOGIN_URL)
+                            .failureHandler((request, response, exception) -> response.setStatus(HttpStatus.UNAUTHORIZED.value()))
+                            .permitAll();
+                })
+                .logout(logOutConfig -> {
+                    logOutConfig.logoutSuccessUrl("/")
+                            .permitAll()
+                            .logoutUrl(UrlContext.LOGOUT_URL)
+                            .permitAll();
+                })
+                .oauth2Login(config -> config.userInfoEndpoint(
+                        oAuth2 -> oAuth2.userService(oAuth2UserService)
+                ))
+                .csrf(configure -> configure.ignoringAntMatchers("/api/**"))
+                .cors(httpSecurityCorsConfigurer -> {
+                    httpSecurityCorsConfigurer.configurationSource(request -> {
+                        CorsConfiguration configuration = new CorsConfiguration();
+                        // TODO cors 허용 url 별도 환경변수화 시키기
+                        configuration.setAllowedOrigins(List.of(
+                                "http://localhost:8080",
+                                "http://localhost:3000"
+                        ));
+                        configuration.setAllowedMethods(List.of(CorsConfiguration.ALL));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(List.of(CorsConfiguration.ALL));
 
-					return configuration;
-				});
-			});
+                        return configuration;
+                    });
+                });
 
-		return httpSecurity.build();
-	}
+        return httpSecurity.build();
+    }
 
-	@Bean
-	public UserDetailsService userDetailsService(MemberService memberService) {
-		return username ->
-			memberService.searchMember(username)
-				.map(SimpleBoardPrincipal::from)
-				.orElseThrow(() -> new UsernameNotFoundException("Not Found Member : " + username));
-	}
+    @Bean
+    public UserDetailsService userDetailsService(MemberService memberService) {
+        return username ->
+                memberService.searchMember(username)
+                        .map(SimpleBoardPrincipal::from)
+                        .orElseThrow(() -> new UsernameNotFoundException("Not Found Member : " + username));
+    }
 
-	@Bean
-	public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(MemberService memberService, PasswordEncoder passwordEncoder) {
-		DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(MemberService memberService, PasswordEncoder passwordEncoder) {
+        DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
 
-		return userRequest -> {
-			OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(userRequest);
-			KakaoOAuth2UserResponse kakaoOAuth2UserResponse = KakaoOAuth2UserResponse.from(oAuth2User.getAttributes());
+        return userRequest -> {
+            OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(userRequest);
+            KakaoOAuth2UserResponse kakaoOAuth2UserResponse = KakaoOAuth2UserResponse.from(oAuth2User.getAttributes());
 
-			MemberDto memberDto = memberService.searchMember(kakaoOAuth2UserResponse.kakaoAccount().email())
-				.orElseGet(() -> memberService.saveMember(
-					generateTemporaryIdAndMailOfKakaoAccount(kakaoOAuth2UserResponse.id()),
-					kakaoOAuth2UserResponse.kakaoAccount().profile().nickname(),
-					passwordEncoder.encode("{bcryp}dummy")
-				));
+            MemberDto memberDto = memberService.searchMember(kakaoOAuth2UserResponse.kakaoAccount().email())
+                    .orElseGet(() -> memberService.saveMember(
+                            generateTemporaryIdAndMailOfKakaoAccount(kakaoOAuth2UserResponse.id()),
+                            kakaoOAuth2UserResponse.kakaoAccount().profile().nickname(),
+                            passwordEncoder.encode("{bcryp}dummy")
+                    ));
 
-			return SimpleBoardPrincipal.from(memberDto);
-		};
-	}
+            return SimpleBoardPrincipal.from(memberDto);
+        };
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
-	private String generateTemporaryIdAndMailOfKakaoAccount(long kakaoRequestId) {
-		return "kakao_oauth2_account_" + String.valueOf(kakaoRequestId);
-	}
+    private String generateTemporaryIdAndMailOfKakaoAccount(long kakaoRequestId) {
+        return "kakao_oauth2_account_" + String.valueOf(kakaoRequestId);
+    }
 }
