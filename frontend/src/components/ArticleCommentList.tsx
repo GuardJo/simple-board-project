3a03c98c5d28ed9d90da173afacf66b9cbda0196
Kyeongho <dagh1218@naver.com>
2024-10-06 20:07:28 +0900

    #78 댓글이 존재하지 않을 경우 관련 문구 미출력 문제 수정
    - 조건 설정 오류로 인한 문구 미출력 문제 수정

--- frontend/src/components/ArticleCommentList.tsx
+++ frontend/src/components/ArticleCommentList.tsx
@@ -1,11 +1,11 @@
 import { CommentInfo } from "@/interface";
 import ArticleComment from "./ArticleComment";
 
-export default function ArticleCommentList({ data }: ArticleCommentListParams = {}) {
+export default function ArticleCommentList({ data = []}: ArticleCommentListParams = {}) {
     return (
         <div>
             <dl className="w-full text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
-                {(data === undefined ? <p>등록된 댓글이 없습니다.</p> : data.map((comment) => (
+                {(data.length === 0 ? <p>등록된 댓글이 없습니다.</p> : data.map((comment) => (
                     <div key={comment.id} className="flex flex-col">
                         <ArticleComment key={comment.id} author={comment.creator} content={comment.content} updatedAt={comment.createTime}></ArticleComment>
                     </div>
