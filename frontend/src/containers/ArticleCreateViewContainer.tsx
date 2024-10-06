f8966e14fac86a1b831508e0a942efd81e281c47
Kyeongho <dagh1218@naver.com>
2024-10-06 20:04:15 +0900

    #78 게시글 상세 컴포넌트 내 게시글 수정 모드 구현
    - ArticleDetailViewContainer 내 버튼 이벤트에 따라 게시글 수정 화면으로 변경되도록 기능 추가
    - 관련하여 비슷한 컴포넌트인 ArticleCreateViewContainer 내 요소들을 ArticleUpdateFormContainer 로 통합
        - ArticleDetailViewContainer와 ArticleCreateViewContainer 모두 해당 컴포넌트를 사용하도록 개선
    - 또한 기존 TextInput 컴포넌트에서 기본 value 값 지정 기능이 없었기에 추가
    - 관련 스토리북 추가

--- frontend/src/containers/ArticleCreateViewContainer.tsx
+++ frontend/src/containers/ArticleCreateViewContainer.tsx
@@ -1,53 +1,7 @@
-"use client"
-
-import ArticleContentForm from "@/components/ArticleContentForm";
-import BasicCard from "@/components/BasicCard";
-import Button from "@/components/Button";
-import TextInput from "@/components/TextInput";
-import { createArticle } from "@/service/ArticleCreateService";
-import { me } from "@/service/LoginService";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import ArticleUpdateFormContainer from "./ArticleUpdateFormContainer";
 
 export default function ArticleCreateViewContainer() {
-    const [titleValue, setTitleValue] = useState("");
-    const [contentValue, setContentValue] = useState("");
-    const router = useRouter();
-
-    useEffect(() => {
-        async function checkLogin() {
-            const response = await me();
-
-            if (!response.ok) {
-                router.push("/login");
-            }
-        }
-        checkLogin();
-    }, []);
-
-    const saveArticle = async () => {
-        await createArticle({
-            title: titleValue,
-            content: contentValue,
-        });
-        router.push("/");
-    };
-
-    const handleTitleValue = (arg: string) => {
-        setTitleValue(arg);
-    };
-
     return (
-        <div className="flex justify-center items-center flex-col gap-3 px-24 py-10">
-            <div className="flex flex-col gap-3 h-full" style={{width: "50%"}}>
-                <BasicCard title="신규 게시글">
-                    <TextInput labelName="제목" onChange={handleTitleValue} placeholder="제목을 입력해 주세요."></TextInput>
-                    <ArticleContentForm onChange={setContentValue} canWrite></ArticleContentForm>
-                </BasicCard>
-                <div className="flex justify-end">
-                    <Button onClick={saveArticle}>저장</Button>
-                </div>
-            </div>
-        </div>
+        <ArticleUpdateFormContainer/>
     );
 }
--- frontend/src/containers/ArticleDetailViewContainer.tsx
+++ frontend/src/containers/ArticleDetailViewContainer.tsx
@@ -6,33 +6,43 @@ import ArticleInfo from "@/components/ArticleInfo";
 import BasicCard from "@/components/BasicCard";
 import Button from "@/components/Button";
 import { ArticleDetailInfo } from "@/interface";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import ArticleCreateViewContainer from "./ArticleCreateViewContainer";
+import ArticleUpdateFormContainer from "./ArticleUpdateFormContainer";
 
-export default function ArticleDetailViewContainer({articleId, articleDetail}: ArticleDetailViewContainerParams) {
-    const router = useRouter();
+export default function ArticleDetailViewContainer({ articleId, articleDetail }: ArticleDetailViewContainerParams) {
+    const [isUpdateMode, setIsUpdateMode] = useState(false);
     const { article, comments, isOwner }: ArticleDetailInfo = articleDetail;
 
-    const handleMoveUpdateViewPage = () => {
-        router.push("/update-view");
+    const handleUpdateMode = (updateMode: boolean) => {
+        setIsUpdateMode(updateMode);
     }
 
+    useEffect(() => {}, [isUpdateMode]);
+
     return (
-        <div className="flex justify-center gap-3 px-24 py-10">
-            <div className="flex flex-col gap-3 h-full" style={{width: "50%"}}>
-                <BasicCard title={article.title}>
-                    <ArticleContentForm content={article.content}></ArticleContentForm>
-                </BasicCard>
-                <BasicCard title={'댓글 (' + comments.length + ')'}>
-                    <ArticleCommentList data={comments} />
-                </BasicCard>
-            </div>
-            <div className="flex flex-col h-96 gap-3" style={{width: "30%"}}>
-                <BasicCard title="게시글 정보">
-                    <ArticleInfo author={article.creator} createdTime={article.createTime} hashtagNames={article.hashtags.map(hashtag => hashtag.hashtagName)}/>
-                </BasicCard>
-                {(isOwner) ? <Button onClick={() => {handleMoveUpdateViewPage()}}>수정</Button> : null}
-            </div>
-        </div >
+        <>
+            {isUpdateMode
+                ?
+                <ArticleUpdateFormContainer articleId={articleId} title={article.title} content={article.content}/>
+                :
+                <div className="flex justify-center gap-3 px-24 py-10">
+                    <div className="flex flex-col gap-3 h-full" style={{ width: "50%" }}>
+                        <BasicCard title={article.title}>
+                            <ArticleContentForm content={article.content}></ArticleContentForm>
+                        </BasicCard>
+                        <BasicCard title={'댓글 (' + comments.length + ')'}>
+                            <ArticleCommentList data={comments} />
+                        </BasicCard>
+                    </div>
+                    <div className="flex flex-col h-96 gap-3" style={{ width: "30%" }}>
+                        <BasicCard title="게시글 정보">
+                            <ArticleInfo author={article.creator} createdTime={article.createTime} hashtagNames={article.hashtags.map(hashtag => hashtag.hashtagName)} />
+                        </BasicCard>
+                        {(isOwner) ? <Button onClick={() => { handleUpdateMode(true) }}>수정</Button> : null}
+                    </div>
+                </div >}
+        </>
     )
 }
 
